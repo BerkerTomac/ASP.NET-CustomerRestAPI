c1ccc3aae749fdc2fd8dfe2c2c4ef332d887ace7
BerkerTomac <berker.tomac@tedu.edu.tr>
2023-04-08 21:45:22 +0300

    initial commit

---
+++ problem/Controllers/SurController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace problem.Controllers
+{
+    [ApiController]
+    //[Route("api/Home")]
+    [Route("Surname")]
+    public class SurController : Controller
+    {
+        // GET: /<controller>/
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        [HttpGet(Name = "GetSur")]
+        public string GetSurname()
+        {
+            string surname = "Tomac";
+            return surname;
+        }
+
+    }
+}
+
